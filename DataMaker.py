73db859bd9ffb55a66d8fb37a352e10f29754a0c
Mid Hunter <midhunterx@gmail.com>
2023-10-24 20:00:31 +0530

    🍔 Equalized return data

--- DataMaker.py
+++ DataMaker.py
@@ -14,69 +14,63 @@ def main():
     pdf_file_list = getPdfFileList(input_dir)
     if pdf_file_list:
         for pdf_file in pdf_file_list:
-
             file_name = os.path.basename(pdf_file)
             print(f"\n==== {file_name} ====")
-
             institution = getInstitutionDetailsPdf(pdf_file)
-            inst_name = institution["name"]
-            inst_place = institution["place"]
-            inst_number = institution["number"]
-            inst_email = institution["email"]
-            print(f"{inst_name},{inst_place},{inst_number},{inst_email}")
-
+            printInstitution(institution)
             student_data = getStudentDetailsPdf(pdf_file)
-            for entry in student_data:
-                name, standard, ifsc, acc_no, holder, branch = entry
-                print(f"{name},{standard},{ifsc},{acc_no},{holder},{branch}")
-
-    # --------------------- PDF TO CSV DATA CONVERSION --------------------- #
-
-            with open(csv_file, mode="a", newline="") as file:
-                writer = csv.writer(file)
-                # Institution details
-                for value in institution.values():
-                    writer.writerow([value])
-                # Student details
-                for row in student_data:
-                    writer.writerow(row)
+            printInstitution(institution)
+            writeToCSV(csv_file, institution, student_data)
 
     # ======================== DOCX FILE PROCESSING ======================== #
 
     docx_file_list = getDocxFileList(input_dir)
     if docx_file_list:
         for docx_file in docx_file_list:
-
             file_name = os.path.basename(docx_file)
             print(f"\n==== {file_name} ====")
-
             institution = getInstitutionDetailsDocx(docx_file)
-            inst_name = institution["name"]
-            inst_place = institution["place"]
-            inst_number = institution["number"]
-            inst_email = institution["email"]
-            print(f"{inst_name},{inst_place},{inst_number},{inst_email}")
-
+            printInstitution(institution)
             student_data = getStudentDetailsDocx(docx_file)
-            for key, value in student_data.items():
-                name = value[0]
-                standard = value[1]
-                ifsc = value[2]
-                acc_no = value[3]
-                holder = value[4]
-                branch = value[5]
-                print(f"{name},{standard},{ifsc},{acc_no},{holder},{branch}")
+            printStudentData(student_data)
+            writeToCSV(csv_file, institution, student_data)
 
-    # --------------------- DOCX TO CSV DATA CONVERSION --------------------- #
 
-            with open(csv_file, mode="a", newline="") as file:
-                writer = csv.writer(file)
-                # Institution details
-                for value in institution.values():
-                    writer.writerow([value])
-                # Student details
-                for row in student_data.values():
-                    writer.writerow(row)
+# =============================== PROCEDURES ================================ #
+
+
+def writeToCSV(csv_file, institution, student_data):
+    """
+    Parameter: (csv_file, institution, student_data)
+    Returns: CSV File in working directory
+    """
+    with open(csv_file, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        # Institution details
+        for value in institution.values():
+            writer.writerow([value])
+        # Student details
+        for row in student_data.values():
+            writer.writerow(row)
+
+
+def printInstitution(institution):
+    inst_name = institution["name"]
+    inst_place = institution["place"]
+    inst_number = institution["number"]
+    inst_email = institution["email"]
+    print(f"{inst_name},{inst_place},{inst_number},{inst_email}")
+
+
+def printStudentData(student_data):
+    for key, value in student_data.items():
+        name = value[0]
+        standard = value[1]
+        ifsc = value[2]
+        acc_no = value[3]
+        holder = value[4]
+        branch = value[5]
+        print(f"{name},{standard},{ifsc},{acc_no},{holder},{branch}")
 
 
 # ================================ FUNCTIONS ================================ #
@@ -85,32 +79,41 @@ def main():
 def getStudentDetailsPdf(pdf_file):
     """
     Parameter: PDF File
-    Returns: A list of lists containing student table data
-    [['NAME', 'CLASS', 'IFSC', 'ACC NO', 'ACC HOLDER', 'BRANCH'],
-    ['Sophia', '1', '7afc5b', '9988 7766554433', 'Isabel', 'Rwanda'],
-    ['Ina', '2', '80afb1', '6 54138546513135', 'Elmer', 'Anguilla'],
-    ['Ophelia', '3', 'bf 841e', '387465354116', 'Marian', 'Bahrain']]
+    Returns: A dictionary of tuples with Student details
+
+    data = {
+        0: (name, standard, ifsc, acc_no, holder, branch),
+        1: (name, standard, ifsc, acc_no, holder, branch),
+        2: (name, standard, ifsc, acc_no, holder, branch)
+    }
     """
     with pdfplumber.open(pdf_file) as pdf:
         for page in pdf.pages:
             # Generate CSV list from PDF table
             table = page.extract_table()
-
-            # Replace \n substring with space
+            data = {}
+            i = 0
             if table:
-                cleaned_table = []
                 for row in table:
+                    # Replace \n substring with space
                     cleaned_row = []
                     for cell in row:
                         if isinstance(cell, str):
                             cleaned_row.append(cell.replace('\n', ' '))
                         else:
                             cleaned_row.append(cell)
-                    cleaned_table.append(cleaned_row)
-                table = cleaned_table
-    # Remove Header in List
-    table.pop(0)
-    return table
+
+                    name = cleaned_row[0]
+                    standard = cleaned_row[1]
+                    ifsc = cleaned_row[2]
+                    acc_no = cleaned_row[3]
+                    holder = cleaned_row[4]
+                    branch = cleaned_row[5]
+
+                    # Extracted data
+                    data[i] = name, standard, ifsc, acc_no, holder, branch
+                    i = i + 1
+    return data
 
 
 def getInstitutionDetailsPdf(pdf_file):
