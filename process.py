e3273f7c7d0d62a06970e7259284d6b321ee4796
Mid Hunter <midhunterx@gmail.com>
2023-11-22 18:15:15 +0530

    🔧 Fixed Other States not in Spreadsheet

--- process.py
+++ process.py
@@ -1549,40 +1549,62 @@ def generateOutputSpreadsheet(db_file, district, xlsx_filename):
     wb = Workbook()
     ws = wb.active
 
-    schoolSQL = """
-    SELECT * FROM Schools WHERE District = ?
-    """
+    # --------------------------------------------------------- [ SQL Queries ]
+
+    if district != "Unknown":
+        schoolSQL = """
+        SELECT * FROM Schools WHERE District = ?
+        """
+        cursor.execute(schoolSQL, (district,))
+
+    elif district == "Unknown":
+        schoolSQL = """
+        SELECT *
+        FROM Schools
+        WHERE District NOT IN (
+        "Thiruvananthapuram", "Kollam", "Pathanamthitta", "Alappuzha",
+        "Kottayam", "Idukki", "Ernakulam", "Thrissur", "Palakkad",
+        "Malappuram", "Kozhikode", "Wayanad", "Kannur", "Kasargod" );
+        """
+        cursor.execute(schoolSQL)
+
     studentSQL = """
     SELECT * FROM Students WHERE SchoolID = ?
     """
-
-    cursor.execute(schoolSQL, (district,))
     schools_table = cursor.fetchall()
 
-# DISTRICT HEADING
-    ws.append([district.upper()])
+    # -------------------------------------------------------- DISTRICT HEADING
+    if district != "Unknown":
+        ws.append([district.upper()])
+    elif district == "Unknown":
+        ws.append(["OTHER STATES"])
     row_number = ws.max_row
     for cell in ws[row_number]:
         cell.font = Font(bold=True, size=28)
     ws.append([""])
 
-# TABLE HEADER
-    excel_header = ["Institution", "Name", "Class", "AccNo", "Amount"]
+    # ------------------------------------------------------------ TABLE HEADER
+    excel_header = ["Contacts", "Name", "Class", "AccNo", "Branch", "Amount"]
     ws.append(excel_header)
     row_number = ws.max_row
     for cell in ws[row_number]:
         cell.font = Font(bold=True, size=16)
 
+    # ----------------------------------------------------- INSTITUTION DETAILS
     for school in schools_table:
         school_id = school[0]
         school_name = school[1]
+        school_dist = school[2]
         school_place = school[3]
         school_no = school[4]
         school_mail = school[5]
         school_name_long = f"{school_name}, {school_place}"
 
-        # INSTITUTION DETAILS
-        if len(school_name_long) < 47:
+        if district == "Unknown":
+            school_name = f"{school_name} ({school_dist})"
+            school_name_long = f"{school_name}, {school_place} ({school_dist})"
+
+        if len(school_name_long) < 45:
             ws.append([school_name_long])
         else:
             ws.append([school_name])
@@ -1594,6 +1616,7 @@ def generateOutputSpreadsheet(db_file, district, xlsx_filename):
         if school_mail:
             ws.append([school_mail])
 
+        # ----------------------------------------------------- STUDENT DETAILS
         cursor.execute(studentSQL, (school_id,))
         student_table = cursor.fetchall()
         for student in student_table:
@@ -1602,15 +1625,15 @@ def generateOutputSpreadsheet(db_file, district, xlsx_filename):
             # student_IFSC = student[4]
             st_accno = student[5]
             # st_holder = student[6]
-            # st_branch = student[7]
+            st_branch = student[7]
 
             amount = convertStdToAmount(st_class)
             st_class = convertNumToStd(st_class)
 
-            # STUDENT DETAILS
-            excel_student_row = ["", st_name, st_class, st_accno, amount]
-            ws.append(excel_student_row)
+            excel_st_row = ["", st_name, st_class, st_accno, st_branch, amount]
+            ws.append(excel_st_row)
 
+        # Space after each school entry
         ws.append([""])
 
     wb.save(xlsx_filename)
