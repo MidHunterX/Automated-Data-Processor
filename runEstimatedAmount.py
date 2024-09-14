9446d33b7fb49b1316b0edf037ec372d864eff89
MidHunterX <midhunterx@gmail.com>
2024-09-14 21:52:14 +0530

    🔧 Fix: OS Independent Estimation Path

--- runEstimatedAmount.py
+++ runEstimatedAmount.py
@@ -1,31 +1,39 @@
 import function as fn
 from function import var
+from pathlib import Path
 
 
 def main():
+    """
+    Gets all files from every District dirs and generates total estimated amount
+    """
     total_amt = 0
 
     district_dataset = var["district_dataset"]
     input_dir = var["input_dir"]
 
     file_list = []
+    input_dir_path = Path(input_dir)
+
     for district in district_dataset:
-        dist_list = fn.getFileList(f"{input_dir}\\{district}", [".docx"])
-        if dist_list:
-            file_list.append(dist_list)
+        district_path = input_dir_path / district
+        dist_file_list = fn.getFileList(str(district_path), [".docx"])
+
+        if dist_file_list:
+            file_list.append(dist_file_list)
 
     for dist_files in file_list:
         for file in dist_files:
             print(file)
             student_data = fn.getStudentDetails(file)
 
-            for key, value in student_data.items():
+            for _, value in student_data.items():
                 standard = value[1]
                 standard = fn.convertStdToNum(standard)
                 amt = fn.convertStdToAmount(standard)
                 total_amt += amt
 
-    print(f"Final Amount: {total_amt}")
+    print(f"Estimated Amount: {total_amt}")
     return 0
 
 
